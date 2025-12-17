6aaa37d5b0105f1d93a8cf0bb8541a0f6def328e
OliverBorgstrom <oliver.borgstrom@sundsvall.se>
2025-12-17 08:15:15 +0100

    feat: cypress tests

--- frontend/cypress/utils/stakeholder.ts
+++ frontend/cypress/utils/stakeholder.ts
@@ -1,13 +1,137 @@
-import { mockStakeholder } from 'cypress/fixtures/mockStakeholder';
+import { mockManualEditStakeholder, mockStakeholder } from 'cypress/fixtures/mockStakeholder';
 
-export const addStakeholder = () => {
-  cy.intercept('GET', `**/citizen/person/${Cypress.env('mockPersonNumber')}`, mockStakeholder).as(
+export const addStakeholder = (role: string) => {
+  cy.intercept('GET', `**/citizen/person/${Cypress.env('mockPersonNumber')}`, { ...mockStakeholder, role: role }).as(
     'getPersonByPersonNumber'
   );
-  cy.get('[data-cy="person-number-input"]').first().type(Cypress.env('mockPersonNumber'));
+  cy.intercept('GET', `**/citizen/person/${Cypress.env('mockNonExistentPersonNumber')}`, {
+    statusCode: 204,
+    data: {},
+  }).as('getEmptyPerson');
+
+  //Personnumber
+  cy.get('[data-cy="person-number-input"]').type('PERSONNUMBER');
+  cy.get('button').contains('Sök').should('exist').click();
+  cy.get('[data-cy="person-number-error').should('be.visible');
+  cy.get('[data-cy="person-number-input"]').clear().type(Cypress.env('mockNonExistentPersonNumber'));
+  cy.get('button').contains('Sök').should('exist').click();
+  cy.wait('@getEmptyPerson');
+  cy.get('[data-cy="empty-person-error').should('be.visible');
+  cy.get('button[aria-label="Rensa"]').should('be.visible').click();
+  cy.get('[data-cy="empty-person-error').should('not.exist');
+  cy.get('[data-cy="person-number-input"]').clear().type(Cypress.env('mockInvalidDatePersonNumber'));
+  cy.get('button').contains('Sök').should('exist').click();
+  cy.get('[data-cy="person-number-error').should('be.visible');
+  cy.get('button[aria-label="Rensa"]').should('be.visible').click();
+  cy.get('[data-cy="person-number-error').should('not.exist')
+  cy.get('[data-cy="person-number-input"]').clear().type(Cypress.env('mockPersonNumber'));
   cy.get('button').contains('Sök').should('exist').click();
   cy.wait('@getPersonByPersonNumber');
-  cy.get('[data-cy="stakeholder-email-input"]').should('exist').type(Cypress.env('mockEmail'));
-  cy.get('[data-cy="stakeholder-mobilephone-input"]').should('exist').type(Cypress.env('mockPhoneNumber'));
+
+  //Email
+  cy.get('[data-cy="person-number-error').should('not.exist');
+  cy.get('[data-cy="email-input-error').should('not.exist');
+  cy.get('[data-cy="phone-number-input-error').should('not.exist');
+  cy.get('[data-cy="stakeholder-email-input"]').should('exist').type('EMAIL');
   cy.get('button').contains('Lägg till person').should('exist').click();
+  cy.get('[data-cy="email-input-error').should('be.visible');
+  cy.get('[data-cy="stakeholder-email-input"]').should('exist').clear().type(Cypress.env('mockEmail'));
+
+  //Phone
+  cy.get('[data-cy="stakeholder-mobilephone-input"]').should('exist').type('PHONENUMBER');
+  cy.get('[data-cy="phone-number-input-error').should('be.visible');
+  cy.get('[data-cy="stakeholder-mobilephone-input"]').should('exist').clear().type(Cypress.env('mockPhoneNumber'));
+  cy.get('button').contains('Lägg till person').should('exist').click();
+};
+
+export const manuallyAddStakeholder = () => {
+  cy.get('[data-cy="manual-person-modal"]').should('exist');
+
+  //No errors
+  cy.get('[data-cy="firstName-input-error"]').should('not.exist');
+  cy.get('[data-cy="lastName-input-error"]').should('not.exist');
+  cy.get('[data-cy="modal-email-input-error"]').should('not.exist');
+  cy.get('[data-cy="modal-phone-input-error"]').should('not.exist');
+
+  //Errors after first try to submit
+  cy.get('[data-cy="modal-add-person-button"]').should('exist').click();
+  cy.get('[data-cy="firstName-input-error"]').should('be.visible');
+  cy.get('[data-cy="lastName-input-error"]').should('be.visible');
+  cy.get('[data-cy="modal-email-input-error"]').should('not.exist');
+  cy.get('[data-cy="modal-phone-input-error"]').should('not.exist');
+
+  //PersonNumber
+  cy.get('[data-cy="modal-personNumber-input"]').should('have.attr', 'readOnly');
+
+  //Name
+  cy.get('[data-cy="modal-firstName-input"]').type('Test');
+  cy.get('[data-cy="modal-lastName-input"]').type('Testsson');
+  cy.get('[data-cy="firstName-input-error"]').should('not.exist');
+  cy.get('[data-cy="lastName-input-error"]').should('not.exist');
+
+  //Email
+  cy.get('[data-cy="modal-email-input"]').type('test');
+  cy.get('[data-cy="modal-add-person-button"]').should('exist').click();
+  cy.get('[data-cy="modal-email-input-error"]').should('be.visible');
+  cy.get('[data-cy="modal-email-input"]').clear().type(Cypress.env('mockEmail'));
+  cy.get('[data-cy="modal-email-input-error"]').should('not.exist');
+
+  //Phone
+  cy.get('[data-cy="modal-phone-input"]').type('Testsson');
+  cy.get('[data-cy="modal-add-person-button"]').should('exist').click();
+  cy.get('[data-cy="modal-phone-input-error"]').should('be.visible');
+  cy.get('[data-cy="modal-phone-input"]').clear().type(Cypress.env('mockPhoneNumber'));
+  cy.get('[data-cy="modal-phone-input-error"]').should('not.exist');
+
+  //Address
+  cy.get('[data-cy="modal-address-input"]').type('Address');
+  cy.get('[data-cy="modal-careOf-input"]').type('careOf');
+  cy.get('[data-cy="modal-zipCode-input"]').type('123 45');
+  cy.get('[data-cy="modal-city-input"]').type('city');
+};
+
+export const manuallyEditStakeholder = () => {
+  cy.get('[data-cy="manual-person-modal"]').should('exist');
+
+  //No errors
+  cy.get('[data-cy="firstName-input-error"]').should('not.exist');
+  cy.get('[data-cy="lastName-input-error"]').should('not.exist');
+  cy.get('[data-cy="modal-email-input-error"]').should('not.exist');
+  cy.get('[data-cy="modal-phone-input-error"]').should('not.exist');
+
+  //PersonNumber
+  cy.get('[data-cy="modal-personNumber-input"]').should('have.value', Cypress.env('mockHyphenPersonNumber'));
+
+  //Name
+  cy.get('[data-cy="modal-firstName-input"]').should('have.value', mockStakeholder.firstName).clear();
+  cy.get('[data-cy="modal-add-person-button"]').should('exist').click();
+  cy.get('[data-cy="firstName-input-error"]').should('exist');
+  cy.get('[data-cy="modal-firstName-input"]').type(mockManualEditStakeholder.firstName || '');
+  cy.get('[data-cy="firstName-input-error"]').should('not.exist');
+  cy.get('[data-cy="modal-lastName-input"]').should('have.value', mockStakeholder.lastName).clear();
+  cy.get('[data-cy="modal-add-person-button"]').should('exist').click();
+  cy.get('[data-cy="lastName-input-error"]').should('exist');
+  cy.get('[data-cy="modal-lastName-input"]').type(mockManualEditStakeholder.lastName || '');
+  cy.get('[data-cy="lastName-input-error"]').should('not.exist');
+
+  //Email
+  cy.get('[data-cy="modal-email-input"]').should('have.value', Cypress.env('mockEmail')).clear().type('test');
+  cy.get('[data-cy="modal-add-person-button"]').should('exist').click();
+  cy.get('[data-cy="modal-email-input-error"]').should('be.visible');
+  cy.get('[data-cy="modal-email-input"]').clear().type(Cypress.env('mockEmail'));
+  cy.get('[data-cy="modal-email-input-error"]').should('not.exist');
+  cy.get('[data-cy="modal-email-input"]').clear()
+
+  //Phone
+  cy.get('[data-cy="modal-phone-input"]').should('have.value', Cypress.env('mockCountryCodePhoneNumber')).type('Testsson');
+  cy.get('[data-cy="modal-add-person-button"]').should('exist').click();
+  cy.get('[data-cy="modal-phone-input-error"]').should('be.visible');
+  cy.get('[data-cy="modal-phone-input"]').clear().type(Cypress.env('mockPhoneNumber'));
+  cy.get('[data-cy="modal-phone-input-error"]').should('not.exist');
+
+  //Address
+  cy.get('[data-cy="modal-address-input"]').should('have.value', mockStakeholder.address).clear().type(mockManualEditStakeholder.address || '');
+  cy.get('[data-cy="modal-careOf-input"]').should('have.value', mockStakeholder.careOf).clear().type(mockManualEditStakeholder.careOf || '');
+  cy.get('[data-cy="modal-zipCode-input"]').should('have.value', mockStakeholder.zipCode).clear().type(mockManualEditStakeholder.zipCode || '');
+  cy.get('[data-cy="modal-city-input"]').should('have.value', mockStakeholder.city).clear().type(mockManualEditStakeholder.city || '');
 };
